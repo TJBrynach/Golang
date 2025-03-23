58f42c666aa54a18be03841741e0ada6baf93d60
Bob <57762966+TJBrynach@users.noreply.github.com>
2025-03-23 17:33:52 +0000

    tview updates

--- CCY Converter/tui.go
+++ CCY Converter/tui.go
@@ -7,18 +7,37 @@ import (
 )
 
 func gui() {
-	box := tview.NewBox().SetBorder(true).SetTitle("Hello, world!")
-	if err := tview.NewApplication().SetRoot(box, true).Run(); err != nil {
+	// box := tview.NewBox().SetBorder(true).SetTitle(" CCY Converter ")
+	app := tview.NewApplication()
+
+	bases := []string{"GBP", "USD", "SGD", "EUR", "NOK", "SEK", "HKD"}
+	// var selectedBase string
+
+	form := tview.NewForm().
+		AddDropDown("Select Base currency (hit enter): ", bases, 0, nil).
+		AddInputField("Amount in base currency to be converted: ", "", 20, nil, nil).
+		AddButton("Convert", nil).
+		AddButton("Quit", func() { app.Stop() })
+	form.SetBorder(true).SetTitle("CCY Converter").SetTitleAlign(tview.AlignLeft)
+
+	_, selectedBase := form.GetFormItem(0).(*tview.DropDown).GetCurrentOption()
+
+	if err := app.SetRoot(form, true).SetFocus(form).EnableMouse(true).Run(); err != nil {
 		panic(err)
 	}
 
-	base := "USD"
-	rates, err := getExchangeRates(base)
+	// base := "USD"
+	rates, err := getExchangeRates(selectedBase)
 	if err != nil {
 		return
 	}
 
+	far := "GBP"
+
 	for key, value := range rates.ConversionRates {
-		fmt.Println(key, value)
+		if key == far {
+			fmt.Println(selectedBase, key, value)
+		}
+
 	}
 }

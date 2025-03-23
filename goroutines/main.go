171940adae7c3862ae208940bacdbc8b1818d4fa
TJBrynach <tjbshorney@gmail.com>
2025-03-23 23:21:21 +0000

    goroutines

--- goroutines/main.go
+++ goroutines/main.go
@@ -1,5 +1,22 @@
 package main
 
-func main() {
+import (
+	"fmt"
+)
 
+func worker(id int, ch chan string) {
+	ch <- fmt.Sprintf("Worker %d done", id)
+}
+
+func main() {
+	// initiate unbuffered int channel
+	ch := make(chan string)
+
+	for i := 0; i <= 3; i++ {
+		go worker(i, ch)
+	}
+
+	for i := 1; i <= 3; i++ {
+		fmt.Println(<-ch)
+	}
 }

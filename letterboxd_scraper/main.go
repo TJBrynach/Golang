dbc08e3edd6459f65b7640ce542c3b77eb943db0
TJBrynach <tjbshorney@gmail.com>
2025-03-30 21:35:56 +0100

    will revisit this project - for now walking passed

--- letterboxd_scraper/main.go
+++ letterboxd_scraper/main.go
@@ -6,13 +6,21 @@ import (
 	"github.com/gocolly/colly"
 )
 
-// we use colly to scrape and store prices for products on amazon
-// takes an amazon product url
-// scrapes products price
-// checks price against target value
-// if price has dropped send notification
+// we use colly to scrape and scrape tmdb
+
+type Film struct {
+	Title       string
+	UserScore   int
+	Director    string
+	Description string
+	ReleaseYear int
+}
 
 func main() {
+	var (
+		movies []Film
+		// mu     sync.Mutex
+	)
 	// Instantiate default collector
 	c := colly.NewCollector()
 	// c.Visit("https://www.amazon.co.uk/Amazon-streaming-device-supports-Vision/dp/B0CJL4J6FG?ref=dlx_20564_dg_dcl_B0CJL4J6FG_dt_mese3_cc_pi&pf_rd_r=E4H2V4RM05DGTYZH7YHV&pf_rd_p=b5b598aa-de73-4068-82b4-c1b94b3594cc")
@@ -27,10 +35,21 @@ func main() {
 		fmt.Println("❌ Request failed:", err)
 	})
 
-	c.OnHTML("html", func(r *colly.HTMLElement) {
-		fmt.Println("there is text")
-		// whole_value := r.FirstChild.Data
-		// fmt.Println(whole_value)
+	c.OnHTML("a.image", func(r *colly.HTMLElement) {
+		r.Request.Visit(r.Attr("href"))
+
 	})
-	c.Visit("https://letterboxd.com/")
+
+	c.OnHTML("span.release_date", func(e *colly.HTMLElement) {
+		year := e.Text
+		fmt.Println(year)
+
+	})
+
+	c.Visit("https://www.themoviedb.org/movie")
+
+	for _, movie := range movies {
+		fmt.Println(movie.Title, movie.Director)
+	}
+
 }

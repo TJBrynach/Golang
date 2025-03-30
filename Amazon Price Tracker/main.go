eae4e6c7fa3d0b11e34897f0da81574f6032c90b
TJBrynach <tjbshorney@gmail.com>
2025-03-30 16:10:51 +0100

    updates to amazon tracker

--- Amazon Price Tracker/main.go
+++ Amazon Price Tracker/main.go
@@ -1,6 +1,10 @@
 package main
 
-import "github.com/gocolly/colly"
+import (
+	"fmt"
+
+	"github.com/gocolly/colly"
+)
 
 // we use colly to scrape and store prices for products on amazon
 // takes an amazon product url
@@ -9,7 +13,13 @@ import "github.com/gocolly/colly"
 // if price has dropped send notification
 
 func main() {
+	// Instantiate default collector
 	c := colly.NewCollector()
 
+	c.OnResponse(func(r *colly.Response) {
+		fmt.Printf("scraping: ", r.StatusCode)
+	})
+	// Start scraping on https://hackerspaces.org
+	c.Visit("https://www.amazon.co.uk/")
+
 }
-https://go-colly.org/docs/examples/basic/

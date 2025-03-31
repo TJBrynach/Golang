e21751609a28754540ecc0928eb2df48eab6082d
TJBrynach <tjbshorney@gmail.com>
2025-03-31 20:51:52 +0100

    testing updates

---
+++ testing/test.py
@@ -0,0 +1,8 @@
+
+
+def triangle():
+    for x in range(1,6):
+        print(x*"*")
+
+
+triangle()
---
+++ testing/testing.go
@@ -0,0 +1,22 @@
+package main
+
+import "testing"
+
+//func TestXxx(t *testing.T)
+
+func Add(a, b int) int {
+	return a + b
+}
+
+func TestAdd(t *testing.T) {
+	result := Add(2, 3)
+	expected := 5
+
+	if result != expected {
+		t.Errorf("Add(2, 3) = %d; want %d", result, expected)
+	}
+}
+
+func main() {
+
+}

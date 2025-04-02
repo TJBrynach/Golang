bfacdd8e775e155ab5c976cfdade709cb8d91bc7
TJBrynach <tjbshorney@gmail.com>
2025-04-02 12:06:20 +0200

    Milan updates to web server
    milan

--- Web Server/main.go
+++ Web Server/main.go
@@ -2,16 +2,31 @@ package main
 
 import (
 	"fmt"
-	"log"
+	"io"
 	"net/http"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
+// for both functions we accept two arguments http.ResponseWriter and a *http.Request value.
+//
+// the *http.Request is used to get information about the request being sent, such as the body being sent in the POST request or info about client
+
+func getRoot(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
+	io.WriteString(w, "Velcome to my website")
+}
+
+func sayHello(w http.ResponseWriter, r *http.Request) {
+	fmt.Printf("got /hello request")
+	io.WriteString(w, "hello gang")
 }
 
 func main() {
 
-	http.HandleFunc("/", handler)
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	http.HandleFunc("/", getRoot)
+	http.HandleFunc("/hello", sayHello)
+	err := http.ListenAndServe(":8080", nil)
+
+	if err != nil {
+		fmt.Println("Error serving the port :8080", err)
+	}
 }
